f3b811fee5d603dce4eb3236985a1ed71c40e199
lizandro castillo <liiizandrocastillo@gmail.com>
2025-01-24 17:36:11 -0400

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,12 @@ function App() {
         <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
           <Navbar routes={routes} />
         </div>
-      )
-      }
+      )}
       <Routes>
         {routes.map(
           ({ path, element }, key) =>
             element && <Route key={key} exact path={path} element={element} />
         )}
-        <Route path="*" element={<Navigate to="https://lilzandro.github.io/proacademy/" replace />} />
       </Routes>
     </>
   );
