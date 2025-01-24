8eb94e8aca989e40a4f46c238075d55cfc2da03e
lizandro castillo <liiizandrocastillo@gmail.com>
2025-01-24 17:40:48 -0400

    dsd

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ import "../public/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/proacademy/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
